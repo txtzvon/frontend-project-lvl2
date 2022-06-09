8548e8473631f2154a789d1abab766c9e310c009
Dmitry Zvonarev <zvonarev.dmitry@yandex.ru>
2022-06-10 00:00:55 +0300

    Added arguments and options for gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,14 +3,40 @@
 import { Command } from 'commander';
 const program = new Command();
 
-program
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
-  program.command('split')
-  .description('Split a string into substrings and display as an array')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
-  });
+const command = () => {
+  console.log('Hello, World!');
+};
 
-program.parse();
+program
+  .version('0.0.1')
+  .option('-f, --format <type>','output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action(command)
+  .parse(process.argv);
+
+
+
+
+
+
+
+
+
+
+
+
+// program
+//   .description('Compares two configuration files and shows a difference.')
+//   .version('0.0.1')
+// 
+
+
+//   program.command('split')
+//   .description('Split a string into substrings and display as an array')
+//   .action((str, options) => {
+//     const limit = options.first ? 1 : undefined;
+//     console.log(str.split(options.separator, limit));
+//   });
+
+// program.parse();
